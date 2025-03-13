11cfd585a0fca3496a9279195dff9ae1e9672613
NJJeus <N.J.Jeus@yandex.ru>
2025-03-13 18:00:34 +0300

    Refactor plot_coverage.sh

--- scripts/plot_coverage.sh
+++ scripts/plot_coverage.sh
@@ -1,45 +1,66 @@
+#!/bin/bash
 
-for i in "$@"
-do
-case $i in
-    -b=*|--bamfile=*)
-    BAMFILE="${i#*=}"
-    ;;
-    -l=*|--listfile=*)
-    LISTFILE="${i#*=}"
-    ;;
-    -f=*|--reference=*)
-    REFERENCE="${i#*=}"
-    ;;
-    -o=*|--output_dir=*)
-    DIR="${i#*=}"
-    ;;
-    --default)
-    DEFAULT=YES
-    ;;
-    *)
-            # unknown option
-    ;;
-esac
+# Function to display usage information
+usage() {
+    echo "Usage: $0 -b|--bamfile <BAMFILE> -l|--listfile <LISTFILE> -f|--reference <REFERENCE> -o|--output_dir <OUTPUT_DIR> [--default]"
+    exit 1
+}
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -b=*|--bamfile=*)
+            BAMFILE="${1#*=}"
+            ;;
+        -l=*|--listfile=*)
+            LISTFILE="${1#*=}"
+            ;;
+        -f=*|--reference=*)
+            REFERENCE="${1#*=}"
+            ;;
+        -o=*|--output_dir=*)
+            OUTPUT_DIR="${1#*=}"
+            ;;
+        --default)
+            DEFAULT=YES
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+    shift
 done
 
-BAMNAME=$(echo $(basename $BAMFILE) |  cut -d '.' -f 1)
+# Validate required arguments
+if [[ -z "$BAMFILE" || -z "$LISTFILE" || -z "$REFERENCE" || -z "$OUTPUT_DIR" ]]; then
+    echo "Error: Missing required arguments."
+    usage
+fi
 
-mkdir -p $DIR
+# Extract base name of the BAM file
+BAMNAME=$(basename "$BAMFILE" .bam)
 
-cat $LISTFILE | while read line; 
-do
-	species=$(echo $line | cut -f 1 -d ',');
-	rname=$(echo $line | cut -f 2 -d ',');
-	endpos=$(echo $line | cut -f 3 -d ',');
-    echo $BAMFILE  $species $rname $endpos;
-	timeout 20 bamsnap \
-		-draw coordinates bamplot coverage base \
-		-bam $BAMFILE \
-		-out "${DIR}/${species}/${rname}.png"\
-		-pos "${rname}:552-${endpos}" \
-		-ref $REFERENCE -width 2000 -height 1000 -read_thickness 10 -read_gap_height 5 -title_fontsize 25 \
-		-bamplot coverage read \
-		-draw coordinates bamplot -no_target_line \
-        -title "${BAMNAME}_${species}_${rname}";
-done
+# Create output directory
+mkdir -p "$OUTPUT_DIR"
+
+# Process each line in the list file
+while IFS=',' read -r species rname endpos; do
+    # Create species-specific directory
+    species_dir="${OUTPUT_DIR}/${species}"
+    mkdir -p "$species_dir"
+
+    # Run bamsnap command
+    timeout 20 bamsnap \
+        -draw coordinates bamplot coverage base \
+        -bam "$BAMFILE" \
+        -out "${species_dir}/${rname}.png" \
+        -pos "${rname}:552-${endpos}" \
+        -ref "$REFERENCE" \
+        -width 2000 -height 1000 \
+        -read_thickness 10 -read_gap_height 5 \
+        -title_fontsize 25 \
+        -bamplot coverage read \
+        -draw coordinates bamplot -no_target_line \
+        -title "${BAMNAME}_${species}_${rname}"
+done < "$LISTFILE"
